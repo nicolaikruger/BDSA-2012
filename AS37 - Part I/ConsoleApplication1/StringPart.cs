7856051751954f160b9316a19e9fff78d8d9ad06
Nicolai Krüger <nkrk@itu.dk>
2012-09-19 16:12:55 +0200

    commented AS37 - Part I

--- AS37 - Part I/ConsoleApplication1/StringPart.cs
+++ AS37 - Part I/ConsoleApplication1/StringPart.cs
@@ -7,10 +7,21 @@ namespace BDSA12
 {
 	struct StringPart
 	{
+		/// <summary>
+		/// The text this string part consists of
+		/// </summary>
 		public readonly String txt;
 
+		/// <summary>
+		/// The color of this string part
+		/// </summary>
 		public readonly ConsoleColor color;
 
+		/// <summary>
+		/// Creates a instance of a StringPart
+		/// </summary>
+		/// <param name="txt">The text this stringPart consists of</param>
+		/// <param name="c">The color of this stringPart</param>
 		public StringPart(String txt, ConsoleColor c)
 		{
 			this.txt = txt;
