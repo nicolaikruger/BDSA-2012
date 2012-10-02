0772f0ba521f615dec81dadfd33a39896aa66b82
Nicolai Krüger <nkrk@itu.dk>
2012-10-02 14:57:21 +0200

    The code for AS_39

---
+++ CalendarSystem/CalendarSystem/TechnicalServices/Storing/ExternalStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalendarSystem.TechnicalServices.Storing
+{
+	//This class will use the .ics format to store and retrieve a Calendar on a external source like google calendar. 
+	//How this is done, will be implemented on a later stage.
+	class ExternalStorage
+	{
+	}
+}
---
+++ CalendarSystem/CalendarSystem/TechnicalServices/Storing/LocalStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CalendarSystem.Domain;
+
+namespace CalendarSystem.TechnicalServices.Storing
+{
+	static class LocalStorage
+	{
+		public static void storeLocally(Calendar cal)
+		{
+			// Stores the calender in a pre-specified format.
+		}
+
+		public static void storeLocally(Calendar cal, StorageFormat formatter)
+		{
+			// Stores the calender in a format specified by the storageFormat.
+		}
+
+		public static void deleteCalendar(Calendar cal)
+		{
+			// Deletes the specified calendar from the local system.
+		}
+	}
+}
