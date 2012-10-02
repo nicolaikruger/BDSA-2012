a5450afddaf7bd4f3df2e1e8e8cece579e46ab44
Andersbrorup <abrj@itu.dk>
2012-10-02 15:07:40 +0200

    Moved some files and created the AS_39

---
+++ Hand in's/AS_39/CalendarSystem/Domain/Calendar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CalendarSystem.TechnicalServices.Storing;
+
+namespace CalendarSystem.Domain
+{
+	class Calendar
+	{
+		public int ID { get; private set; }
+		public String Name { get; set; }
+		private int nextID = 1;
+
+		public void create(DateTime StartDateAndTime, DateTime EndDateAndTime, string Location, string Description, string Title)
+		{
+			int id = findId();
+			saveEvent(id, StartDateAndTime, EndDateAndTime, Location, Description, Title);
+		}
+
+		private void saveEvent(int id, DateTime StartDateAndTime, DateTime EndDateAndTime, string Location, string Description, string Title)
+		{
+			Event ev = new Event(id, StartDateAndTime, EndDateAndTime, Location, Description, Title);
+
+			// Saves the event to a collection - maybe a hashset?
+
+			LocalStorage.storeLocally(this);
+		}
+
+		private int findId()
+		{
+			return nextID++;
+		}
+
+		public void edit(int id, DateTime StartDateAndTime, DateTime EndDateAndTime, string Location, string Description, string Title)
+		{
+			saveEvent(id, StartDateAndTime, EndDateAndTime, Location, Description, Title);
+		}
+
+		public void delete(int id)
+		{
+			//TODO
+		}
+	}
+}
---
+++ Hand in's/AS_39/CalendarSystem/Domain/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalendarSystem.Domain
+{
+	class Event
+	{
+		public int Id;
+		public DateTime CreatedAt, StartDateAndTime, EndDateAndTime;
+		public string Location, Description, Title;
+
+
+		public Event(int Id, DateTime StartDateAndTime, DateTime EndDateAndTime, string Location, string Description, string Title)
+		{
+			this.Id = Id;
+			this.StartDateAndTime = StartDateAndTime;
+			CreatedAt = DateTime.Now;
+			this.EndDateAndTime = EndDateAndTime;
+			this.Location = Location;
+			this.Description = Description;
+			this.Title = Title;
+		}
+	}
+}
